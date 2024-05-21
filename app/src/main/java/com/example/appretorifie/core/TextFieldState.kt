90cc5b18f7c49d3d2ed44536dec5a13a24a35ea1
saav97 <sa.arriagada97@gmail.com>
2024-05-21 20:01:34 -0300

    textFieldState

---
+++ app/src/main/java/com/example/appretorifie/core/TextFieldState.kt
@@ -0,0 +1,4 @@
+package com.example.appretorifie.core
+
+class TextFieldState {
+}
--- app/src/main/java/com/example/appretorifie/ui/model/NoteViewModel.kt
+++ app/src/main/java/com/example/appretorifie/ui/model/NoteViewModel.kt
@@ -1,5 +1,27 @@
 package com.example.appretorifie.ui.model
+import android.app.Application
+import androidx.lifecycle.LiveData
+import com.example.appretorifie.db.NotesDatabase
+import com.example.appretorifie.db.model.Note
+import com.example.appretorifie.repository.NotesRepository
 
-class NoteViewModel {
+class NoteViewModel(application: Application) {
+
+    private var repository: NotesRepository
+        get() {
+            TODO()
+        }
+    var all: LiveData<List<Note>>
+        get() {
+            TODO()
+        }
+
+    init {
+        val db = NotesDatabase.getInstance(application)
+        val dao = db.notesDao()
+        repository = NotesRepository(dao)
+
+        all = repository.getNotes()
+    }
 
 }
